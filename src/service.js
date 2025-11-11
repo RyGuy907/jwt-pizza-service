8daec514788212a2748dcbe8b1a15fff241997aa
Ryan Russon <ryan.russon7@gmail.com>
2025-11-11 16:25:16 -0700

    fix

--- src/service.js
+++ src/service.js
@@ -57,7 +57,7 @@ app.use('*', (req, res) => {
   });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   if (logger.logError) {
     logger.logError(err, {
       method: req.method,
